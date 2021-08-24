f2b3f7927e49af5c980f04571a723fbeacfd81b8
alexboluch <alex.boluch.u@gmail.com>
2021-08-24 11:58:10 +0300

    Add host in settings

--- test_task/settings.py
+++ test_task/settings.py
@@ -29,7 +29,7 @@ SITE_URL = secret.SITE_URL
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
