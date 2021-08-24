7a4e61153357eb3df8e791352c59a23fc3ef8d6c
alexboluch <alex.boluch.u@gmail.com>
2021-08-24 12:42:53 +0300

    Update urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('get-info/', views.get_info_gituser),
     path('view-info/', views.get_view),
+    path('', views.get_info_gituser),
 ]
